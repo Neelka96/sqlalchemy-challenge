f423ffa235ecc6ffe468e272db20ff3c7a52e70c
Neel Agarwal <neelkagarwal@gmail.com>
2025-02-05 23:55:57 -0600

    Implement Flask routes and database queries for precipitation data

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,73 @@
 # Import the dependencies.
-from flask import Flask
 from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.automap import automap_base
+
+from flask import Flask, jsonify
+import datetime as dt
+
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine('sqlite:///Resources/hawaii.sqlite')
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with = engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+Session = sessionmaker(bind = engine)
 
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route('/')
+def index():
+    '''Lists all available API Routes'''
+    return(
+        '<h1>Hawaii Weather Analysis</h1><br>'
+        '<h2>Available API Routes:</h2><br>'
+        '<a href="/api/v1.0/precipitation"></a><br>'
+        '<a href=""></a><br>'
+        '<a href=""></a><br>'
+    )
+
+@app.route('/api/v1.0/precipitation')
+def precipitation_query():
+    '''Precipitation query results for last year'''
+    index()
+    with Session() as session:  # Find the most recent date in the data set.
+        first_date = session.query(
+            func.max(measurement.date)
+            ).scalar()
+    first_date = dt.datetime.strptime(first_date, '%Y-%m-%d')
+    last_date = first_date - dt.timedelta(days = 365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    sel = [
+        measurement.date
+        ,measurement.prcp
+    ]
+    with Session() as session:
+        data = session.query(*sel
+            ).filter(measurement.date <= first_date
+            ).filter(measurement.date >= last_date
+            ).all()
+    
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug = True)
